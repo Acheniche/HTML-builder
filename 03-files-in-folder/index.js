56bdd8f262a36da05c46dfe0689cac9ca1557c6a
Acheniche <99502683+Acheniche@users.noreply.github.com>
2023-05-08 20:36:55 +0300

    feat: add solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, "secret-folder"), {withFileTypes: true}, (err, data) =>{
+if(err){
+    console.log(err);
+}
+data.forEach(file =>{
+    if(file.isFile()){
+        let name = file.name.split(".");
+        fs.stat(path.join(__dirname, "secret-folder", file.name), (err,stats) =>{
+            console.log(name[0] + '   ' + name[1] + '   ' + stats.size + 'kB');
+        } );
+    }
+});
+});
