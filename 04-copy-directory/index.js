41e1781652b5e7e0e7902788ef86720c5129f84e
Acheniche <99502683+Acheniche@users.noreply.github.com>
2023-05-09 14:13:48 +0300

    Feat: fix bug

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
+fs.rmdir(path.join(__dirname, "files-copy"), { recursive: true }, (err) => {});
 fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, (err) => {});
 
 fs.readdir(
